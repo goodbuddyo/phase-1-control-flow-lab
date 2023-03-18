bdb86e4d8e6ecdce8fcd130e682098041a306534
Seano <sean01234@yahoo.com>
2023-03-17 22:21:39 -0400

    add if, ternary and switch

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return 'This one is on me!';
+  } else if (someValue > 400 && someValue <= 2000) {
+    return 'That will be twenty bucks.';
+  } else if (someValue > 2000 && someValue <= 2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city) {
+  const taxiMessage = city === "NYC" ? "Ok, sounds good." : "No go.";
+  console.log(city);
+  console.log(taxiMessage);
+  return taxiMessage;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  let tipResponse;
+
+  switch (tip) {
+    case 'generous':
+      tipResponse = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      tipResponse = 'Thank you.';
+      break;
+    default:
+      tipResponse = "Bye."
+      break;
+  }
+  console.log(tipResponse);
+  return tipResponse
 }
+
+scuberGreetingForFeet(155);
+ternaryCheckCity("NYC");
+ternaryCheckCity("LA");
+switchOnCharmFromTip("generous");
+switchOnCharmFromTip("not as generous");
+switchOnCharmFromTip("something else");
